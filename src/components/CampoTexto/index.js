bdebef72b2a0550c7897b16a43d2d88370bd211c
João Vitor de Abreu Sousa <jvabreusousa14@gmail.com>
2022-11-02 22:31:34 -0300

    feat: terceiro modulo {useState}

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,22 +1,32 @@
 
 // function CampoTexto(){
 
-// }
+//}
 
-import './CampoTexto.css'
+// import { useState } from 'react';
+import './CampoTexto.css'; 
 
 //ARROW FUNCTION
 const CampoTexto = (props) => {
 
+    // let nominho = 'João Vitor'
+
+    //CONFORME O VALOR MUDA, O MEU ESTADO MUDA JUNTO
+    //E MEU ESTADO MUDANDO JUNTO O REACT SABE QUE ELE DEVE REEINDERIZAR O COMPONENTE 
+    //const [valor, setValor] = useState('João Vitor'); 
+
+    const aoDigitado = (e) => {
+        props.aoAlterado(e.target.value); 
+    }
 
     const placeholderModificada = `${props.placeholder}...`
-
+    
     return(
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={placeholderModificada}/>
+            <input onChange={aoDigitado} value={props.valor}  required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,14 +1,72 @@
+import { useState } from 'react'; 
 import './Formulario.css'
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
 
-const Formulario = () => {
+const Formulario = (props) => {
+
+    const times = [
+        'Programação', 
+        'Front-end', 
+        'Data Science', 
+        'DevOps', 
+        'Mobile', 
+        'Inovação e Gestão'
+    ]
+
+
+    //VARÍÁVEIS QUE POSSO PEGAR ESSES VALORES E TRABALHAR COM ELES 
+    //COMUNICAÇÃO ENTRE OS COMPONENTES
+    const [nome, setNome] = useState(''); 
+    const [cargo, setCargo] = useState(''); 
+    const [imagem, setImage] = useState(''); 
+    const [time, setTime] = useState('Programação'); 
+
+
+
+    //OBJETO
+    const aoSalvar = (e) => {
+        e.preventDefault(); 
+        props.aoColaboradorCadastrado({
+            nome: nome, 
+            cargo: cargo, 
+            imagem: imagem,
+            time: time
+        })
+    }
+
     return (
         <section className="formulario" >
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar os dados do colaborador!</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
-                <CampoTexto label="Imagem" placeholder="Imagem de perfil" />
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Nome" 
+                    placeholder="Digite o seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Cargo" 
+                    placeholder="Digite o seu cargo"
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)} />
+                <CampoTexto 
+                    label="Imagem" 
+                    placeholder="Imagem de perfil"
+                    valor={imagem}
+                    aoAlterado={valor => setImage(valor)}
+                />
+                <ListaSuspensa 
+                    label="Time" 
+                    itens={times} 
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}/>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
